677acddccd23d61cb0ae2a272fde4fe9c0417e07
Belfegorrr <pelichek.iago@gmail.com>
2023-03-30 09:59:27 -0300

    função somar da calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,42 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from "vue";
+
+// import { createApp } from 'vue';
+
+const estado = reactive({
+  num1: "",
+  num2: "",
+  soma: "0",
+});
+
+function soma(event) {
+  estado.soma = parseInt(estado.num1) + parseInt(estado.num2);  
+  console.log(estado.soma)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <hr />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h1>Resutlado: {{ estado.soma }}</h1>
+  <label>Calculadora EBAC</label>
+  <br />
+  <input type="number" @change=" estado.num1 = e.target.value" />
+  <br />
+  <input type="number" @change=" estado.num2 = e.target.value" />
+  <br />
+  <button @click="soma">Somar</button>
+  <br />
+  <br />
+  <!-- <div>Selected: {{ selected }}</div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option>Somar</option>
+    <option>Subtrair</option>
+    <option>Multiplicar</option>
+    <option>Dividir</option>
+  </select> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
